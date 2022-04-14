71a0cc19b8bdc9980f6af2cbd9d202bf5398c3bd
kbr <life10181110>
2022-04-14 21:43:21 +0900

    plife 화면 띄우기 테스트를 위해 war -> jar 변경

--- lifeProject/src/main/java/com/plife/web/ServletInitializer.java
+++ lifeProject/src/main/java/com/plife/web/ServletInitializer.java
@@ -7,7 +7,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		// 테스트
 		return application.sources(LifeProjectApplication.class);
 	}
 
---
+++ lifeProject/src/main/java/com/plife/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.plife.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "plife";
+	}
+}
