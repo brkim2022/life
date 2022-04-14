723d1fbfd6713e8030a1882753ceed88c9103bb2
kbr <life10181110>
2022-04-14 20:53:52 +0900

    first commit

---
+++ lifeProject/src/main/java/com/plife/web/LifeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.plife.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeProjectApplication.class, args);
+	}
+
+}
---
+++ lifeProject/src/test/java/com/plife/web/LifeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plife.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
